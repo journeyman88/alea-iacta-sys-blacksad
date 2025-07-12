39ff6e330cb5c273e4e9b8221de88aa801fbf284
Marco Bignami <journeyman@unknown-domain.no-ip.net>
2025-07-12 21:33:02 +0200

    Rework for cdi

--- src/main/java/net/unknowndomain/alea/systems/blacksad/BlacksadCommand.java
+++ src/main/java/net/unknowndomain/alea/systems/blacksad/BlacksadCommand.java
@@ -15,39 +15,44 @@
  */
 package net.unknowndomain.alea.systems.blacksad;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import java.util.Locale;
 import java.util.Optional;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.unknowndomain.alea.registry.HistoryRegistry;
 import net.unknowndomain.alea.systems.RpgSystemCommand;
 import net.unknowndomain.alea.systems.RpgSystemDescriptor;
 import net.unknowndomain.alea.roll.GenericRoll;
 import net.unknowndomain.alea.systems.RpgSystemOptions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author journeyman
  */
+@Named
+@ApplicationScoped
+@Slf4j
+@NoArgsConstructor
 public class BlacksadCommand extends RpgSystemCommand
 {
-    private static final Logger LOGGER = LoggerFactory.getLogger(BlacksadCommand.class);
     private static final RpgSystemDescriptor DESC = new RpgSystemDescriptor("Blacksad RPG", "sad", "blacksad");
     
-    public BlacksadCommand()
-    {
-        
+    @Inject
+    private Instance<HistoryRegistry> historyRegistry;
+
+    @Override
+    protected Optional<HistoryRegistry> getHistoryRegistry() {
+        return historyRegistry.stream().findFirst();
     }
     
     @Override
     public RpgSystemDescriptor getCommandDesc()
     {
         return DESC;
-    }
-
-    @Override
-    protected Logger getLogger()
-    {
-        return LOGGER;
     }
 
     @Override
